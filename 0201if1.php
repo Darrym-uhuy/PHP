e07477b521174ce83704169d9065865f0d062c42
Muhammad Ridwan <ridwanmuh2009@gmail.com>
2025-01-29 19:44:56 +0700

    tugas 0201if9pilihan

--- 0201if1.php
+++ 0201if1.php
@@ -1,48 +1,32 @@
 <?php
-    $hasil = "lulus";
-    if ($hasil == "lulus") {
-        echo "Selamat anda $hasil";
-    } else {
-        echo "Maaf anda $hasil";
-    }
-
-    echo "<h1>contoh IF dengan 2 pilihan</h1>";
-    if ($hasil == "lulus") {
-        echo "Selamat anda $hasil";
-    } else {
-        echo "Maaf anda $hasil";
-    }
-
-echo "<h1>contoh IF dengan 3 pilihan</h1>";
-# 60-100 Lulus
-# 50-59 Remedial
-# 0-49 Tidak Lulus
-$nilai = 120;
-    echo "<h2>batas bawah</h2>";
-    if ($nilai >= 60) {
-        echo "Selamat anda <b>Lulus</b>. nilai anda $nilai";
-    }elseif ($nilai >= 50) {
-        echo "anda <b>Remedial</b>. nilai anda $nilai";
-    }else{
-        echo "maaf anda <b>Tidak Lulus</b>. nilai anda $nilai";
-}
-    echo "<h2>batas atas</h2>";
-    if ($nilai < 50) {
-        echo "maaf anda <b>Tidak Lulus</b>. nilai anda $nilai";
-    }elseif ($nilai < 60) {
-        echo "anda <b>Remedial</b>. nilai anda $nilai";
-    }else{
-        echo "Selamat anda <b>Lulus</b>. nilai anda $nilai";
-    }
-
+  #IF 9 PILIHAN
+# 81-100 A Bobot 4
+# 78-80,99 A- Bobot 3,7
+# 75-77,99 B+ Bobot 3,3
+# 70-74,99 B Bobot 3
+# 65-69,99 B- Bobot 2,7
+# 60-64,99 C+ Bobot 2,3
+# 55-59,99 C Bobot 2
+# 40-54,99 D Bobot 1
+# 0-39,99 E Bobot 0
+$nilai = 81.8;
     echo "<h2>batas atas dan bawah</h2>";
-    if ($nilai >= 60 AND $nilai <= 100) {
-        echo "Selamat anda <b>Lulus</b>. nilai anda $nilai";
-    }elseif ($nilai >= 50 AND $nilai < 60) {
-        echo "anda <b>Remedial</b>. nilai anda $nilai";
-    }elseif ($nilai >= 0 AND $nilai < 50) {
-        echo "maaf anda <b>Tidak Lulus</b>. nilai anda $nilai";
-    }else{
-        echo"tidak ada kategori untuk nilai $nilai";
+    if ($nilai >= 81 AND $nilai <= 100) {
+        echo "nilai anda $nilai Akreditasi anda <b>A</b> Bobot anda 4";
+    }elseif ($nilai >= 78 AND $nilai < 80.99) {
+        echo " nilai anda $nilai Akreditasi anda <b>A-</b> Bobot anda 3,7";
+    }elseif ($nilai >= 75 AND $nilai < 77.99) {
+        echo " nilai anda $nilai  Akreditasi anda <b>B+</b> Bobot anda 3,7";
+    }elseif ($nilai >= 70 AND $nilai < 74.99) {
+        echo " nilai anda $nilai Akreditasi anda <b>B</b> Bobot anda 3,7";
+    }elseif ($nilai >= 65 AND $nilai < 69.99) {
+        echo " nilai anda $nilai Akreditasi anda <b>B-</b> Bobot anda 3,7";
+    }elseif ($nilai >= 60 AND $nilai < 64.99) {
+        echo " nilai anda $nilai Akreditasi anda <b>C+</b> Bobot anda 3,7";
+    }elseif ($nilai >= 55 AND $nilai < 59.99) {
+        echo " nilai anda $nilai Akreditasi anda <b>C</b> Bobot anda 3,7";
+    }elseif ($nilai >= 40 AND $nilai < 54.99) {
+        echo " nilai anda $nilai Akreditasi anda <b>D</b> Bobot anda 3,7";
+    }elseif ($nilai >= 0 AND $nilai < 39.99) {
+        echo " nilai anda $nilai Akreditasi anda <b>E</b> Bobot anda 3,7";
     }
-
