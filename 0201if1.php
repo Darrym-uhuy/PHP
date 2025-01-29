1609d1f27a48a85ee7e9e260486f240def9d581d
Muhammad Ridwan <ridwanmuh2009@gmail.com>
2025-01-29 19:49:15 +0700

    update tugas 0201if9pilihan

--- 0201if1.php
+++ 0201if1.php
@@ -9,24 +9,24 @@
 # 55-59,99 C Bobot 2
 # 40-54,99 D Bobot 1
 # 0-39,99 E Bobot 0
-$nilai = 81.8;
+$nilai = 41.7;
     echo "<h2>batas atas dan bawah</h2>";
     if ($nilai >= 81 AND $nilai <= 100) {
         echo "nilai anda $nilai Akreditasi anda <b>A</b> Bobot anda 4";
     }elseif ($nilai >= 78 AND $nilai < 80.99) {
         echo " nilai anda $nilai Akreditasi anda <b>A-</b> Bobot anda 3,7";
     }elseif ($nilai >= 75 AND $nilai < 77.99) {
-        echo " nilai anda $nilai  Akreditasi anda <b>B+</b> Bobot anda 3,7";
+        echo " nilai anda $nilai  Akreditasi anda <b>B+</b> Bobot anda 3,3";
     }elseif ($nilai >= 70 AND $nilai < 74.99) {
-        echo " nilai anda $nilai Akreditasi anda <b>B</b> Bobot anda 3,7";
+        echo " nilai anda $nilai Akreditasi anda <b>B</b> Bobot anda 3";
     }elseif ($nilai >= 65 AND $nilai < 69.99) {
-        echo " nilai anda $nilai Akreditasi anda <b>B-</b> Bobot anda 3,7";
+        echo " nilai anda $nilai Akreditasi anda <b>B-</b> Bobot anda 2,7";
     }elseif ($nilai >= 60 AND $nilai < 64.99) {
-        echo " nilai anda $nilai Akreditasi anda <b>C+</b> Bobot anda 3,7";
+        echo " nilai anda $nilai Akreditasi anda <b>C+</b> Bobot anda 2,3";
     }elseif ($nilai >= 55 AND $nilai < 59.99) {
-        echo " nilai anda $nilai Akreditasi anda <b>C</b> Bobot anda 3,7";
+        echo " nilai anda $nilai Akreditasi anda <b>C</b> Bobot anda 2";
     }elseif ($nilai >= 40 AND $nilai < 54.99) {
-        echo " nilai anda $nilai Akreditasi anda <b>D</b> Bobot anda 3,7";
+        echo " nilai anda $nilai Akreditasi anda <b>D</b> Bobot anda 1";
     }elseif ($nilai >= 0 AND $nilai < 39.99) {
-        echo " nilai anda $nilai Akreditasi anda <b>E</b> Bobot anda 3,7";
+        echo " nilai anda $nilai Akreditasi anda <b>E</b> Bobot anda 0";
     }
